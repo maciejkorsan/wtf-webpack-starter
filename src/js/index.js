5bfbb30fd9b5dbd03fd2174a38d73c676f896d8c
Maciej Korsan <maciejkorsan@gmail.com>
2020-06-14 14:29:29 +0200

    chore: PWA is disabled by default

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,8 @@
 import '../scss/main.scss';
 
 // uncomment the lines below to enable PWA
-
-import {registerSW} from './pwa.js';
-registerSW();
+// import {registerSW} from './pwa.js';
+// registerSW();
 
 /* place your code below */
 
